4335ddcf031757fa635a1e419ebda9aa22d1d901
SandeepKomal <sandeepkomalp@gmail.com>
2024-03-18 14:17:52 +0530

    infra-app

---
+++ Infra/tf-modules/tf-k8s/ng.tf
@@ -0,0 +1,19 @@
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.example.name
+  node_group_name = var.cluster_name
+  node_role_arn   = var.node_group_role_arn
+  subnet_ids      = var.subnet_ids
+  capacity_type   = "ON_DEMAND"
+  instance_types  = [var.node_instance_type]
+
+  scaling_config {
+    desired_size = var.node_desired_size
+    max_size     = var.node_max_size
+    min_size     = var.node_min_size
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+}
+
